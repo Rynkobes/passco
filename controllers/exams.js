1a28e0140fc4202a51219f3e11020422c4fe6574
Terryb <terrydagbartels@gmail.com>
2022-10-18 21:01:18 +0000

    boilerplate code modified

--- controllers/exams.js
+++ controllers/exams.js
@@ -1,19 +1,19 @@
-exports.getAllExams = () => {
+exports.getAllExams = (req, res) => {
     console.log(`there are X number of exams on this portal`)
 }
 
-exports.getExamByYear = () => {
+exports.getExamByYear = (req, res) => {
     console.log(`there are x exams for the year 2020`)
 }
 
-exports.getExamByCategory = () => {
+exports.getExamByCategory = (req, res) => {
     console.log(`There are x exams in the $$Category$$ category`)
 }
 
-exports.getExamBySubject = () => {
+exports.getExamBySubject = (req, res) => {
     console.log(`There are 12 math exams`)
 }
 
-exports.getSpecificExam = () => {
+exports.getSpecificExam = (req, res) => {
     console.log(`There you go`)
 }
