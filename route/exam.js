046fb01e2ecf5fd80964510c649325ab626cbf0f
Terryb <terrydagbartels@gmail.com>
2022-10-20 18:10:37 +0000

    restful routes added for exam

--- route/exam.js
+++ route/exam.js
@@ -5,7 +5,12 @@ const router = express.Router()
 const examController = require('../controllers/exams')
 
 router.get('/exams', examController.getAllExams)
-router.get('/exams/id', examController.getSpecificExam)
+router.get('/exams/:id', examController.getSpecificExam)
+router.get('/exams/:year', examController.getExamByYear)
+router.get('/exams/:subject', examController.getExamBySubject)
+router.get('/exams/:category', examController.getExamByCategory)
+router.post('/exams', examController.postExam)
+router.put('/exams/:id/edit', examController.editSpecificExam)
 
 
 module.exports = router
