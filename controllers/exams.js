0afbf3c884f61a42c8df5f5dbf3c033d2d1c84d0
Terryb <terrydagbartels@gmail.com>
2022-10-20 18:07:14 +0000

    get all exam code added seed data for create exam

--- controllers/exams.js
+++ controllers/exams.js
@@ -1,5 +1,9 @@
-exports.getAllExams = (req, res) => {
-    console.log(`there are X number of exams on this portal`)
+const Exam = require('../models/exam')
+
+exports.getAllExams = async (req, res) => {
+    const allExams = await Exam.find({})
+    console.log(allExams)
+    res.send(`looks like we are still not connected`)
 }
 
 exports.getExamByYear = (req, res) => {
@@ -16,4 +20,35 @@ exports.getExamBySubject = (req, res) => {
 
 exports.getSpecificExam = (req, res) => {
     console.log(`There you go`)
+}
+
+exports.editSpecificExam = (req, res) => {
+    console.log(`Edit exam with id __id`)
+}
+
+//Add new exam
+exports.postExam = async(req, res) => {
+    const newExam = await new Exam({
+        subject: "English",
+        Year: 2008,
+        category: 'WASSCE',
+        questions: [{
+            question: 'Most African countries face poverty while few enjoy',
+            options: ['influence', 'money', 'affluence', 'power'],
+            correctAnswer: 'affluence'
+        },
+            {
+                question: 'By failing to attend the interview, Idoko has lost a golden opportunity.',
+                options: ['blessed', 'bright', 'good', 'delightful'],
+                correctAnswer: 'delightful'
+            }
+        ]
+    })
+    newExam.save()
+    console.log(`New Exam added`)
+}
+
+//need the exam id to do this
+exports.putExam = (req, res) => {
+    console.log(`Exam updated`)
 }
