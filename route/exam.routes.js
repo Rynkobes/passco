c118c0ee2e616733a1f4b018b2201d8c7d3e3f7c
Terryb <terrydagbartels@gmail.com>
2022-11-02 20:28:07 +0000

    rename route files to depict purpose

--- route/exam.routes.js
+++ route/exam.routes.js
@@ -5,10 +5,10 @@ const router = express.Router()
 const examController = require('../controllers/exams')
 
 router.get('/exams', examController.getAllExams)
-router.get('/exams/:id', examController.getSpecificExam)
-router.get('/exams/:year', examController.getExamByYear)
-router.get('/exams/:subject', examController.getExamBySubject)
 router.get('/exams/:category', examController.getExamByCategory)
+router.get('/exams/:year', examController.getExamByYear)
+// router.get('/exams/:id', examController.getSpecificExam)
+router.get('/exams/:subject', examController.getExamBySubject)
 router.post('/exams', examController.postExam)
 router.put('/exams/:id/edit', examController.editSpecificExam)
 
