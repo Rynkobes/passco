19d2348128efa3fa7ac90a080ccb13c435227816
Terryb <terrydagbartels@gmail.com>
2022-11-02 20:35:48 +0000

    add auth0 configuration and login route

--- route/auth.js
+++ route/auth.js
@@ -1,10 +1,26 @@
 const express = require('express')
+require('dotenv').config()
+const { auth } = require('express-openid-connect')
 
 const router = express.Router()
-
 const authController = require('../controllers/auth')
 
-router.post('/login', authController.signInUser)
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.SECRET,
+  issuerBaseURL: process.env.ISSUERBASEURL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+
+router.get('/login', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+})
+  
 router.get('/logout', authController.signOut)
 router.post('/signup', authController.signUpUser)
 
