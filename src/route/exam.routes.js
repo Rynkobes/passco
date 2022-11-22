64d9cd1bb1c6216819fcc80e7a18775722752eac
Terryb <terrydagbartels@gmail.com>
2022-11-22 16:46:26 +0000

    fine tune routes to match application purpose.

--- src/route/exam.routes.js
+++ src/route/exam.routes.js
@@ -5,12 +5,14 @@ const router = express.Router()
 const examController = require('../controllers/exams')
 
 router.get('/exams', examController.getAllExams);
-router.get('/exams/?year', examController.getExamByYear);
-router.get('/exams/:category', examController.getExamByCategory);
-router.get('/exams/:id', examController.getSpecificExam)
-router.get('/exams/exam:subject', examController.getExamBySubject)
-router.post('/exams', examController.postExam)
-router.put('/exams/:id/edit', examController.editSpecificExam)
+router.get('/exams/{id}', examController.getSpecificExam)
+router.get('/exams/category/:category', examController.getExamByCategory);
+router.get('/exams/exams?filter=category=category&year=year', examController.getFilteredExam)
+router.post('/exams/id', examController.getExamBySubject)
+router.patch('/exams/id/questions', examController.getExamByYear);
+router.post('/exams/id/answers', examController.putExam)
+router.patch('/exams/id/answer', examController.postExam)
+// router.put('/exams/:id/edit', examController.editSpecificExam)
 
 
 module.exports = router
