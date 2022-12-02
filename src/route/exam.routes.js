3e42a59864f3494d492f75c6f3b13dfd8fd90b03
Terryb <terrydagbartels@gmail.com>
2022-12-02 19:02:49 +0000

    Put filtering on get all exam routes and remove specific filtering routes

--- src/route/exam.routes.js
+++ src/route/exam.routes.js
@@ -4,15 +4,12 @@ const router = express.Router()
 
 const examController = require('../controllers/exams')
 
+//Exam by year, category and subject enabled on '/exams'
 router.get('/exams', examController.getAllExams);
 router.get('/exams/{id}', examController.getSpecificExam)
-router.get('/exams/category/:category', examController.getExamByCategory);
-router.get('/exams/exams?filter=category=category&year=year', examController.getFilteredExam)
-router.post('/exams/id', examController.getExamBySubject)
-router.patch('/exams/id/questions', examController.getExamByYear);
+router.patch('/exams/year?year', examController.getExamByYear)
 router.post('/exams/id/answers', examController.putExam)
 router.patch('/exams/id/answer', examController.postExam)
-// router.put('/exams/:id/edit', examController.editSpecificExam)
 
 
 module.exports = router
