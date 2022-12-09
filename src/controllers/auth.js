7dbbe22d11659e630ded648e093ce55cdeacf7b7
Terryb <terrydagbartels@gmail.com>
2022-12-09 12:51:48 +0000

    remove sign up route, let auth0 handle on login page

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,10 +2,6 @@ exports.signInUser = (req, res) => {
     res.redirect('/login')
 }
 
-exports.signUpUser = (req, res) => {
-    console.log(`You just signed up to the best BECE & WASSCE Preparation portal in the world`)
-}
-
 exports.signOut = (req, res) => {
     console.log(`You've been logged out`)
 }
