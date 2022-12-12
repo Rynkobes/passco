affc7baccf8f92b6bab134bfffe8655084f5b6e5
Terryb <terrydagbartels@gmail.com>
2022-12-12 21:49:21 +0000

    add auth middleware to verify access to routes

--- src/route/exam.routes.js
+++ src/route/exam.routes.js
@@ -1,16 +1,17 @@
 const express = require('express')
-
 const router = express.Router()
 
 const examController = require('../controllers/exams')
+const authController = require('../controllers/auth')
+
 
 //Exam by year, category and subject enabled on '/exams'
-router.get('/exams', examController.getAllExams);
-router.get('/exams/:id', examController.getSpecificExam)
-router.post('/exams/flipcard/:exam_id', examController.answerSpecificQuestion)
-router.patch('/exams/year?year', examController.getExamByYear)
-router.post('/exams/id/answers', examController.putExam)
-router.patch('/exams/id/answer', examController.postExam)
+router.get('/exams',  examController.getAllExams);
+router.get('/exams/:id', authController.jwtCheck, examController.getSpecificExam)
+router.post('/exams/flipcard/:exam_id',authController.jwtCheck, examController.answerSpecificQuestion)
+router.patch('/exams/year?year', authController.jwtCheck, examController.getExamByYear)
+router.post('/exams/id/answers', authController.jwtCheck, examController.putExam)
+router.patch('/exams/id/answer', authController.jwtCheck, examController.postExam)
 
 
 module.exports = router
