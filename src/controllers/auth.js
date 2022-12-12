200e588fa2f6ebced90b201a9275780f7fff8c35
Terryb <terrydagbartels@gmail.com>
2022-12-12 21:47:17 +0000

    add reusable express-jwt middleware for authorisation

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,18 @@
+const {expressjwt :jwt} = require('express-jwt');
+const jwks = require('jwks-rsa');
+
+exports.jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-afvqsvmc.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'http://localhost:3000',
+  issuer: process.env.ISSUERBASEURL,
+  algorithms: ['RS256']
+});
+
 exports.signInUser = (req, res) => {
     res.redirect('/login')
 }
